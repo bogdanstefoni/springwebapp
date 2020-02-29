7f9f6260b29c3ec2063d7d17b3769641fec76369
bogdanstef <bogdanstefoni.wd@gmail.com>
2020-02-29 13:15:21 +0200

    simple spring web app

---
+++ src/main/java/bogdan/spring5webapp/demo/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package bogdan.spring5webapp.demo.controllers;
+
+import bogdan.spring5webapp.demo.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+    }
+}
